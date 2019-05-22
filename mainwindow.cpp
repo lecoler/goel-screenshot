4ea77c0db1e9d7e5e92dc0b864007f39c941f8f5
lecoler <wu870027945@163.com>
2019-05-22 16:52:44 +0800

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow()
 {
-    delete shadeWindow;
+
 }
 //复写按键事件
 void MainWindow::keyPressEvent(QKeyEvent *event)
@@ -37,29 +37,70 @@ void MainWindow::init()
     this->setMinimumSize(screenSize);
     //获取当前屏幕内容并填充屏幕
     bgImg = screen->grabWindow(0);
-//    QPalette palette;
-//    palette.setBrush(QPalette::Background,QBrush(bgImg));
-//    this->setPalette(palette);
 
-    shadeWindow = new QWidget(this);
-    shadeWindow->setGeometry(0,0,width(),height());
-    shadeWindow->show();
 
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    //背景图
+    painter.drawPixmap(0,0,width(),height(),bgImg);
+    //遮罩层
+    QRect full(0,0,width(),height());
+
+    //选择框
+    if(mouseStatus==MOUSE_PRESS){
+
+        QRect select(startPos,endPos);
+        painter.setClipRegion(QRegion(full)-QRegion(select));
+        //边框
+        QRect border(startPos.rx(),startPos.ry(),endPos.rx()-startPos.rx(),endPos.ry()-startPos.ry());
+        painter.setPen(QPen(Qt::green,5,Qt::DashLine));
+        painter.setBrush(QBrush());
+        painter.drawRect(border);
+    }
+    painter.setPen(QPen());
+    painter.setBrush(QBrush(QColor(0,0,0,100)));
+    painter.drawRect(full);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton){
+        mouseStatus = MOUSE_PRESS;
+        pressPos = event->pos();
+    }else if(mouseStatus == MOUSE_RELEASE){
+        close();
+    }else {
+        mouseStatus = MOUSE_RELEASE;
+        update();
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    QPoint movePos = event->pos();
+    QPoint pos = movePos - pressPos;
+    int x = pos.rx();
+    int y = pos.ry();
+    if(x<0 && y<0){
+        startPos = movePos;
+        endPos = pressPos;
+    }else if(x>0 && y<0){
+        startPos = QPoint(pressPos.rx(),movePos.ry());
+        endPos = QPoint(movePos.rx(),pressPos.ry());
+    }else if (x<0 && y>0) {
+        startPos = QPoint(movePos.rx(),pressPos.ry());
+        endPos = QPoint(pressPos.rx(),movePos.ry());
+    }else {
+        startPos = pressPos;
+        endPos = movePos;
+    }
     update();
 }
-//截图
-void MainWindow::PainterEvent(QPaintEvent *event)
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *)
 {
-    //遮罩层
-
-    QPainter painter(this);
-//    QPainter
-    painter.drawPixmap(0,0,width(),height(),bgImg);
-    QRect full(0,0,width(),height());
-    painter.setBrush(QColor(0,0,0));
-
-    painter.drawRect(full);
-//    shadeWindow->setStyleSheet("background-color:rgba(0,0,0,0.5);");
 
 }
-
--- mainwindow.h
+++ mainwindow.h
@@ -9,6 +9,9 @@
 #include <QScreen>
 #include <QWidget>
 
+#define MOUSE_PRESS 1
+#define MOUSE_RELEASE 0
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -17,16 +20,21 @@ public:
     MainWindow(QWidget *parent=0);
     ~MainWindow();
 private:
-    //复写按键事件
-    void keyPressEvent(QKeyEvent *event);
     //复制到剪切板
     void copy(QPixmap);
     //初始化
     void init();
+
+    void keyPressEvent(QKeyEvent *event);
+    void paintEvent(QPaintEvent *);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *);
+    void mouseMoveEvent(QMouseEvent *event);
+
     QPixmap bgImg;
-    QWidget *shadeWindow;
-    //截图
-    void PainterEvent(QPaintEvent *event);
+    unsigned short mouseStatus=0;
+    QPoint startPos,endPos,pressPos;
+
 };
 
 #endif // MAINWINDOW_H

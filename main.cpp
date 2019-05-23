bb85160d1030a4fa57ed537486f3d47765b521e3
lecoler <wu870027945@163.com>
2019-05-23 22:10:37 +0800

    update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     //无边框、置顶
-    w.setWindowFlags(Qt::CustomizeWindowHint|Qt::WindowStaysOnTopHint|Qt::FramelessWindowHint|Qt::WindowSystemMenuHint);
+    w.setWindowFlags(Qt::CustomizeWindowHint|Qt::WindowStaysOnTopHint|Qt::FramelessWindowHint);
     w.setAutoFillBackground(false);
+    w.showFullScreen();
     w.move(0,0);
     w.show();
 
